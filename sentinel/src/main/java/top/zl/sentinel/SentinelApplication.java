1fd0edb7855a3232ffa4c5ed70331db808e6336a
zhangliang <v_liang.zhang1@fowo.com>
2021-07-13 18:48:29 +0800

    初始化、sentinel简单Demo1

---
+++ sentinel/src/main/java/top/zl/sentinel/SentinelApplication.java
@@ -0,0 +1,15 @@
+package top.zl.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zl
+ * 2021/07/13
+ */
+@SpringBootApplication
+public class SentinelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class);
+    }
+}
---
+++ sentinel/src/main/java/top/zl/sentinel/config/SentinelAspectConfiguration.java
@@ -0,0 +1,17 @@
+package top.zl.sentinel.config;
+
+import com.alibaba.csp.sentinel.annotation.aspectj.SentinelResourceAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zl
+ */
+@Configuration
+public class SentinelAspectConfiguration {
+
+    @Bean
+    public SentinelResourceAspect sentinelResourceAspect() {
+        return new SentinelResourceAspect();
+    }
+}
